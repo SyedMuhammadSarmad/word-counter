b1ee01fef759144a6944fc1c3c60306eb8feadc3
SyedMuhammadSarmad <smsarmad92@gmail.com>
2024-07-01 01:56:31 +0500

    addition complete

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-"use strict";
-let str = "hello";
-for (let i of str) {
-    console.log(i);
-}
+import inquirer from "inquirer";
+let { str } = await inquirer.prompt([{
+        name: "str",
+        type: "input",
+        message: "Enter paragraph:"
+    }]);
+let characcters_without_spaces = str.replace(/\s/g, ""); //"helloworld"
+console.log("Characters without spaces :", characcters_without_spaces.length);
+str = str.replace(/[^\w\s]/g, " "); // Replace punctuation with spaces
+let wordsArray = str.split(/\s+/); //['','Hello','','world','']
+let words = wordsArray.filter((word) => word.length > 0); //['hello','world']
+console.log("words count:", words.length);
--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
-let str = "hello"
+import inquirer from "inquirer"
 
-for(let i of str){
-    console.log(i)
-}
+let {str} = await inquirer.prompt([{ //"   Hello   world  "
+    name:"str",
+    type:"input",
+    message:"Enter paragraph:"
+}])
+
+
+let characcters_without_spaces = str.replace(/\s/g,"")//"helloworld"
+console.log("Characters without spaces :",characcters_without_spaces.length)
+
+str = str.replace(/[^\w\s]/g, " "); // Replace punctuation with spaces
+
+let wordsArray = str.split(/\s+/);//['','Hello','','world','']
+
+let words = wordsArray.filter((word:any) => word.length > 0);//['hello','world']
+
+console.log("words count:",words.length);
+
